106e07d8ad7ba641e15cd860e04f5c2251d1fd9a
khaled274 <aly9258@gmail.com>
2023-02-17 18:49:36 +0200

    health care

--- lib/view/login/cubit/login_cubit.dart
+++ lib/view/login/cubit/login_cubit.dart
@@ -18,40 +18,4 @@ class LoginCubit extends Cubit<LoginState> {
         isPassword ? Icons.visibility_outlined : Icons.visibility_off_outlined;
     emit(LoginChangePasswordVisibilityState());
   }
-  //   Future<void> userLogin({
-  //   required String email,
-  //   required String password,
-  // }) async {
-  //   emit(LoginLoadingState());
-  //   final token = await FirebaseMessagingHelper.getToken();
-  //   String os = Platform.operatingSystem;
-  //   print(os);
-  //   final response = await DioHelper.postData(
-  //     url: login,
-  //     data: {
-  //       'email': email,
-  //       'password': password,
-  //       'token': token,
-  //       'serial_number': token,
-  //       'os': os,
-  //     },
-  //   );
-  //   final data = response.data as Map<String, dynamic>;
-
-  //   try {
-  //     print(data);
-  //     userModel = UserModel.fromJson(data);
-  //     if (userModel!.data.token != null) {
-  //       CacheHelper.cacheUserInfo(
-  //           token: userModel!.data.token.toString(), userModel: userModel!);
-  //     }
-  //     emit(LoginSuccessState(userModel: userModel!));
-  //   } on DioError catch (e) {
-  //     debugPrint(e.error.toString());
-  //   } catch (e, s) {
-  //     debugPrint(e.toString());
-  //     debugPrint(s.toString());
-  //     emit(LoginLErrorState(error: e.toString()));
-  //   }
-  // }
 }
